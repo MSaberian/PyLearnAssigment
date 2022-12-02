8a0e1464b94d3489b0409ad5496a22eb51fd0fc5
MSaberian <mo.saberian@yahoo.com>
2022-12-02 14:22:43 +0330

    assigment5v2

--- Assigment5/Assigment5.1.py
+++ Assigment5/Assigment5.1.py
@@ -1,5 +1,12 @@
-import MoSaFunction5
+# import MoSaFunction5
 
 n = int(input("please enter first number: "))
 m = int(input("please enter second number: "))
-MoSaFunction5.chess(n,m)
+# MoSaFunction5.chess(n,m)
+for i in range(n):
+    for j in range(m):
+        if (i%2 + j%2)%2:
+            print("*",end="")
+        else:
+            print("#",end="")
+    print("")
--- Assigment5/Assigment5.3.py
+++ Assigment5/Assigment5.3.py
@@ -1,4 +1,11 @@
-import MoSaFunction5
+# import MoSaFunction5
 
 n = int(input("please enter number: "))
-MoSaFunction5.Rhombus(n)
+# MoSaFunction5.Rhombus(n)
+for i in range(n*2-1):
+    for j in range(n*2-1):
+        if abs(j - (n-1)) <= (n-1 - abs(i - (n-1))):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print("")
