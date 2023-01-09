ba01dcbb0e0e5639bf502617e80635a6344ed19c
MSaberian <mo.saberian@yahoo.com>
2023-01-09 12:42:21 +0330

    rick and morty 2

--- Assigment13/interstellar.py
+++ Assigment13/interstellar.py
@@ -1,3 +1,4 @@
+import time
 import random
 import arcade
 from spaceship import Spaceship
@@ -6,23 +7,33 @@ from earth import Earth
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(width=600, height=400, title='interstellar game ')
+        super().__init__(width=600, height=400, title='Rick and Morty game')
         # arcade.set_background_color(arcade.color.DARK_BLUE)
         self.background = arcade.load_texture('pictures/rick-and-morty-outer-space.jpg')
+        self.start_picture = arcade.load_texture('pictures/rick_and_morty.jpg')
         self.me = Spaceship(self.width, 'mamad')
         self.enemy = Enemy(random.randint(100,self.width-100),self.height - 120)
         self.earth = Earth(self.width - 80, 80, 'earth c137')
+        self.initial = True
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_lrwh_rectangle_textured(0, 0, 600, 400, self.background)            
-        self.me.draw()
-        self.enemy.draw()
-        self.earth.draw()
+        if self.initial:
+            arcade.draw_lrwh_rectangle_textured(0, 0, 600, 400, self.start_picture) 
+            arcade.draw_text('Rick and Morty', 165, 130, arcade.color.DARK_BLUE, 40, 1, 'left', 'Brush Script MT', True)
+            arcade.draw_text('Welcome to MoSa game', 140, 90, arcade.color.DARK_BLUE, 30, 1, 'left', 'Brush Script MT', True)
+            arcade.draw_text('press any key to play', 170, 40, arcade.color.BLACK, 20, 1, 'left')
+        else:
+            arcade.draw_lrwh_rectangle_textured(0, 0, 600, 400, self.background)            
+            self.me.draw()
+            self.enemy.draw()
+            self.earth.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
-        print('symbol key: ',symbol)
-        if symbol == 97: # left 
+        # print('symbol key: ',symbol)
+        if self.initial:
+            self.initial = False
+        elif symbol == 97: # left 
             self.me.center_x -= self.me.speed
         elif symbol == 100: # right
             self.me.center_x += self.me.speed
